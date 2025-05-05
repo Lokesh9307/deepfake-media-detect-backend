888720c6a17d9a8d93754d231f3863df7e438bba
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2025-05-05 23:54:18 +0530

    error solve

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def test():
     return jsonify({"message": "Server is running!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
